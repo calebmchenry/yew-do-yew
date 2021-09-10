9b2e9a3a8e1a25107c3f13e198b62d029159ea74
Caleb McHenry <calebmchenry@gmail.com>
2021-09-09 23:16:14 -0400

    style: add tailwind css

--- src/main.rs
+++ src/main.rs
@@ -58,20 +58,29 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div>
-                <label for="new-todo">{"New TODO"} </label>
-                <input
-                    id="new-todo"
-                    type="text"
-                    value=self.input_value.clone()
-                    oninput=self.link.callback(|e: InputData| Msg::Input(e.value))
-                />
-                <button onclick=self.link.callback(|_| Msg::Add) type="button">{"Add"}</button>
+            <div class=classes!("h-full", "w-full", "max-w-screen-sm", "mx-auto", "pt-10")>
+                    <label class=classes!("block") for="new-todo">{"Write something down 🙂"} </label>
+
+                <div class=classes!("flex-initial", "flex-row")>
+                    <input
+                        id="new-todo"
+                        type="text"
+                        class=classes!("border", "border-gray-400", "p-1", "px-2", "rounded")
+                        placeholder="What needs doing?"
+                        value=self.input_value.clone()
+                        oninput=self.link.callback(|e: InputData| Msg::Input(e.value))
+                    />
+                    <button 
+                        class=classes!("border", "rounded", "bg-blue-600", "text-white", "p-2", "px-4", "leading-4", "border-blue-600", "ml-1")
+                        onclick=self.link.callback(|_| Msg::Add) type="button">
+                            {"Add"}
+                        </button>
+                </div>
                 <ul>
                 { for self.todos.iter().enumerate().map(|(index, todo)| html! {
-                    <li>
-                        <label>
-                            <input type="checkbox" checked=todo.completed onclick=self.link.callback(move |_| Msg::Toggle(index))/>
+                    <li class=classes!("my-2")>
+                        <input type="checkbox" checked=todo.completed onclick=self.link.callback(move |_| Msg::Toggle(index))/>
+                        <label class=classes!("ml-2")>
                             {
                                 match todo.completed {
                                     false => html! {&todo.name},
